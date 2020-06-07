3bf682c6523b6e8077e0bdfc47f0752294f2e539
ytret <ytretyakov18@gmail.com>
2020-06-07 22:39:12 +0300

    Minuscule stylistic changes
    1. Tabs to spaces
    2. # to // in boot.s

--- kernel/arch/x86/boot.s
+++ kernel/arch/x86/boot.s
@@ -1,7 +1,7 @@
 .set MAGIC,      0xE85250D6
-.set ARCH,       0      # i386
+.set ARCH,       0  // i386
 .set HEADER_LEN, header_end - header_start
-.set CHECKSUM, -(MAGIC + ARCH + HEADER_LEN)
+.set CHECKSUM,   -(MAGIC + ARCH + HEADER_LEN)
 
 .section .multiboot
 .align 8
@@ -11,14 +11,14 @@ header_start:
 .long HEADER_LEN
 .long CHECKSUM
 
-# Multiboot2 information request
+// Tag: Multiboot2 information request
 .align 8
-.word 1         # type
-.word 0         # flags
-.long 12        # size
-.long 6         # memory map
+.word 1         // type
+.word 0         // flags
+.long 12        // size
+.long 6         // memory map
 
-# End of tags
+// End of tags
 .align 8
 .word 0
 .word 0
@@ -34,73 +34,73 @@ stack_top:
 .global _entry
 .type _entry, @function
 _entry:
-	cli
-	movl $stack_top, %esp
+    cli
+    movl $stack_top, %esp
 
-	pushl %eax
-	pushl %ebx
-	call load_gdt
-	popl %ebx
-	popl %eax
+    pushl %eax
+    pushl %ebx
+    call load_gdt
+    popl %ebx
+    popl %eax
 
-	pushl %ebp
-	movl %esp, %ebp
-	pushl %ebx
-	pushl %eax
-	call main
-	addl $8, %esp
-	popl %ebp
+    pushl %ebp
+    movl %esp, %ebp
+    pushl %ebx
+    pushl %eax
+    call main
+    addl $8, %esp
+    popl %ebp
 
-	addl $4, %esp
-	popl %ebp
+    addl $4, %esp
+    popl %ebp
 
-	# Hang if main() returns.
-	jmp halt
+    // Hang if main() returns.
+    jmp halt
 .size _entry, . - _entry
 
 .global halt
 .type halt, @function
 halt:
-	cli
-1:	hlt
-	jmp 1b
+    cli
+1:  hlt
+    jmp 1b
 .size halt, . - halt
 
 .local load_gdt
 .type load_gdt, @function
 load_gdt:
-	lgdt gdt
-	ljmp $0x08, $1f
-1:	movw $0x10, %ax
-	movw %ax, %ds
-	movw %ax, %es
-	movw %ax, %fs
-	movw %ax, %gs
-	movw %ax, %ss
-	ret
+    lgdt gdt
+    ljmp $0x08, $1f
+1:  movw $0x10, %ax
+    movw %ax, %ds
+    movw %ax, %es
+    movw %ax, %fs
+    movw %ax, %gs
+    movw %ax, %ss
+    ret
 .size load_gdt, . - load_gdt
 
 gdt:
-# Null segment (GDT descriptor)
-.word end_gdt - gdt	# size
-.long gdt		# offset
+// Null segment (GDT descriptor)
+.word end_gdt - gdt // size
+.long gdt           // offset
 .word 0x0000
 
-# Code segment
-.word 0xFFFF		# limit 0:15
-.word 0x0000		# base 0:15
-.byte 0x00		# base 16:23
-.byte 0x9A		# access byte
-.byte 0xCF		# (flags < 4) | limit 16:19
-.byte 0x00		# base 24:31
+// Code segment
+.word 0xFFFF        // limit 0:15
+.word 0x0000        // base 0:15
+.byte 0x00          // base 16:23
+.byte 0x9A          // access byte
+.byte 0xCF          // (flags < 4) | limit 16:19
+.byte 0x00          // base 24:31
 
-# Data segment
-.word 0xFFFF		# limit 0:15
-.word 0x0000		# base 0:15
-.byte 0x00		# base 16:23
-.byte 0x92		# access byte
-.byte 0xCF		# (flags < 4) | limit 16:19
-.byte 0x00		# base 24:31
+// Data segment
+.word 0xFFFF        // limit 0:15
+.word 0x0000        // base 0:15
+.byte 0x00          // base 16:23
+.byte 0x92          // access byte
+.byte 0xCF          // (flags < 4) | limit 16:19
+.byte 0x00          // base 24:31
 end_gdt:
 
 .global memset
