5415be1cfe45700d707056af6e5ad8966f115323
ytret <ytretyakov18@gmail.com>
2020-06-14 12:49:17 +0300

    Move memset, memcpy and memcmp from boot.s to mem.s

--- kernel/arch/x86/boot.s
+++ kernel/arch/x86/boot.s
@@ -38,9 +38,7 @@ _entry:
     movl $stack_top, %esp
 
     pushl %eax
-    pushl %ebx
     call load_gdt
-    popl %ebx
     popl %eax
 
     pushl %ebp
@@ -102,64 +100,3 @@ gdt:
 .byte 0xCF          // (flags < 4) | limit 16:19
 .byte 0x00          // base 24:31
 end_gdt:
-
-.global memset
-.type memset, @function
-memset:
-    pushl %ebp
-    movl %esp, %ebp
-    pushl %edi
-
-    movl 8(%ebp), %edi
-    movl 12(%ebp), %eax
-    movl 16(%ebp), %ecx
-    rep stosb
-
-    popl %edi
-    popl %ebp
-    ret
-.size memset, . - memset
-
-.global memcpy
-.type memcpy, @function
-memcpy:
-    pushl %ebp
-    movl %esp, %ebp
-    pushl %edi
-    pushl %esi
-
-    movl 8(%ebp), %edi
-    movl 12(%ebp), %esi
-    movl 16(%ebp), %ecx
-    rep movsb
-
-    popl %esi
-    popl %edi
-    popl %ebp
-    ret
-.size memcpy, . - memcpy
-
-.global memcmp
-.type memcmp, @function
-memcmp:
-    pushl %ebp
-    pushl %edi
-    pushl %esi
-
-    movl 8(%ebp), %esi
-    movl 12(%ebp), %edi
-    movl 16(%ebp), %ecx
-    repe cmpsb
-
-    movl $0, %eax
-    jz 1f
-
-    movl $1, %eax
-    ja 1f
-    negl %eax
-
-1:  popl %esi
-    popl %edi
-    popl %ebp
-    ret
-.size memcmp, . - memcmp
