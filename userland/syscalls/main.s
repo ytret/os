528c42aab2b0193759c643a0b8bc076867207d9f
ytret <ytretyakov18@gmail.com>
2021-03-31 21:01:33 +0300

    Add userland/syscalls

---
+++ userland/syscalls/main.s
@@ -0,0 +1,69 @@
+// ytret's OS - hobby operating system
+// Copyright (C) 2020, 2021  Yuri Tretyakov (ytretyakov18@gmail.com)
+//
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+.global _entry
+.type _entry, @function
+_entry:
+    // Set up a stack.
+    movl $.stack_top, %esp
+    xorl %ebp, %ebp
+
+    // Open /dev/chr0.
+    movl $0, %eax
+    movl $.pathname, %ebx
+    movl $9, %ecx
+    int $0x88
+
+    // Save the file descriptor in %ebx.
+    movl %eax, %ebx
+
+    // Any errors?
+    cmpl $0, %ebx
+    jl 2f
+
+    // Read one ASCII character from the console.
+1:  movl $2, %eax
+    movl $.buffer, %ecx
+    movl $1, %edx
+    int $0x88
+    cmpl $0, %eax
+    jl 2f
+
+    // Write that character to the console.
+    movl $1, %eax
+    movl $.buffer, %ecx
+    movl $1, %edx
+    int $0x88
+
+    // Write a newline.
+    // movl $1, %eax
+    // movl $.newline, %ecx
+    // int $0x88
+
+    jmp 1b
+
+2:  jmp 2b
+.size _entry, . - _entry
+
+.section .data
+.pathname:  .ascii "/dev/chr0"
+.buffer:    .skip 1, 0
+.newline:   .ascii "\n"
+
+.section .bss
+.stack_bottom:
+.skip 4096
+.stack_top:
