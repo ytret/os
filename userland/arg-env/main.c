f324c430372f7ddabfd6c8354ffba128372b5c9a
ytret <ytretyakov18@gmail.com>
2021-07-02 13:28:24 +0300

    userland/arg-env: print terminating NULL elements

--- userland/arg-env/main.c
+++ userland/arg-env/main.c
@@ -4,19 +4,21 @@
 int main(int argc, char **argv, char **environ) {
     printf("argc = %d\n", argc);
 
-    for (int i = 0; i < argc; i++) {
+    int i;
+    for (i = 0; i < argc; i++) {
         printf("argv[%d] = %s\n", i, argv[i]);
     }
+    printf("argv[%d] = NULL\n", i);
 
     if (!environ) {
         printf("environ = NULL\n");
     } else {
         printf("environ = 0x%08X\n", (uint32_t) environ);
-        int i;
-        for (i = 0; environ[i] != NULL; i++) {
-            printf("environ[%d] = %s\n", i, environ[i]);
+        int j;
+        for (j = 0; environ[j] != NULL; j++) {
+            printf("environ[%d] = %s\n", j, environ[j]);
         }
-        printf("environ[%d] = NULL\n", i);
+        printf("environ[%d] = NULL\n", j);
     }
 
     fflush(stdout);
